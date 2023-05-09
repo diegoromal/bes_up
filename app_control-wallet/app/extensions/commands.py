384eaa43af7fa5a0f957e6c777340e3080e6a7a7
diegoromal <diego@qosit.com.br>
2023-05-09 12:19:40 -0300

    Deleted directory app_control_wallet

--- app_control-wallet/app/extensions/commands.py
+++
@@ -1,9 +0,0 @@
-from app.extensions.database import db
-
-
-def init_app(app):
-    @app.cli.command()
-    def createdb():
-        """Creates database"""
-        db.create_all()
-        print(app.config['SQLALCHEMY_DATABASE_URI'])
