7c5121fdbb1909ea0b2f05f500f495033de474f0
Diego RA <diego@qosit.com.br>
2023-08-08 08:48:04 -0300

    Organização do Repositório

--- README.md
+++
@@ -1,2 +0,0 @@
-# up_cs_tech-team
-Repository of the Startup Culture subject project at Universidade Positivo
--- control_wallet/app/extensions/commands.py
+++
@@ -1,9 +0,0 @@
-from app.extensions.database import db
-
-
-def init_app(app):
-    @app.cli.command()
-    def createdb():
-        """Creates database"""
-        db.create_all()
-        print('Database created')
