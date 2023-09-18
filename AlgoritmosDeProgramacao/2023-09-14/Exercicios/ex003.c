653ea2b4712d4fea2b1c100aeba673baa7950604
Diego RA <diego@qosit.com.br>
2023-09-18 16:48:28 -0300

    Algoritimos - Ex003

--- AlgoritmosDeProgramacao/2023-09-14/Exercicios/ex003.c
+++ AlgoritmosDeProgramacao/2023-09-14/Exercicios/ex003.c
@@ -6,5 +6,31 @@ Por exemplo, com A = 1 e R = 3, tem-se “1.00 3.00 9.00 27.00 81.00 243.00 729.
 #include <stdio.h>
 
 int main() {
+
+    float A, R, valor_atual=1.0;
+    int i;
+
+    double power(float base, float exponent) {
+        double result = 1.0;
+        int i;
+        for (i = 0; i < exponent; i++) {
+            result *= base;
+        }
+        return result;
+    }
+
+    printf("Entre com o valor inicial (A): ");
+    scanf("%f", &A);
+
+    printf("Entre com o valor da razao (R): ");
+    scanf("%f", &R);
+
+    printf("Com A=%.2f e R=%.2f, tem-se: \n%.2f %.2f ", A, R, A, A * R);
+
+    for (i = 1; i <= 10; i++) {
+        valor_atual = power(R, A + i);
+        printf("%.2f ", valor_atual);
+        }
+
     return 0;
 }
