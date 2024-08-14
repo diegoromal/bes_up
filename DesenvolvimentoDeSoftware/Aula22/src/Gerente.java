a7ced73c201830be5c94efd55365462218770e24
diegoromal <diego@qosit.com.br>
2024-08-14 09:06:08 -0300

    Adiciona o link do notion do material de aula

--- DesenvolvimentoDeSoftware/Aula22/src/Gerente.java
+++ DesenvolvimentoDeSoftware/Aula22/src/Gerente.java
@@ -1,3 +1,35 @@
-public class Gerente {
+public class Gerente extends Funcionario {
+    
+    private String nomeProjeto;
+    private float valorBonus;
+
+    public Gerente(int matricula, String nome, float horasrabalhadas, float valorHora, String nomeProjeto,
+            float valorBonus) {
+        super(matricula, nome, horasrabalhadas, valorHora);
+        this.nomeProjeto = nomeProjeto;
+        this.valorBonus = valorBonus;
+    }
+
+    @Override
+    public void calcularSalarioLiquido() {
+        salarioLiquido = salarioBruto - ir - inss + valorBonus;
+    }
+
+    @Override
+    public String retornarDados(){
+        String txt = "\nMatrícula: " + matricula +
+                     "\nNome do Gerente: " + nome +
+                     "\nNome do projeto: " + nomeProjeto +
+                     "\nValor da hora de trabalho: R$ " + valorHora +
+                     "\nHoras trabalhadas: " + horasrabalhadas +
+                     "\nValor do Bônus: R$ " + valorBonus +
+                     "\nIR do mês: R$ " + ir +
+                     "\nINSS do mês: R$ " + inss +
+                     "\nFGTS recolhido: " + fgts +
+                     "\nSalário Líquido: R$ " + salarioLiquido;
+
+        return txt;
+    }
+
     
 }
---
+++ DesenvolvimentoDeSoftwareVisual/NomeDoProjeto/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite a sua idade: ");
+
+int idade = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Sua idade é: " + idade);
