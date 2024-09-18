d972cfc9e59a9aa31ccea6e6b840af1abd7e99f5
diegoromal <diego@qosit.com.br>
2024-09-18 10:16:08 -0300

    Ajustes no método cadastrar, implementado método buscar

--- DesenvolvimentoDeSoftwareVisual/api/api/Program.cs
+++ DesenvolvimentoDeSoftwareVisual/api/api/Program.cs
@@ -1,40 +1,54 @@
-using System.Text.Json;
 using api.Entities;
-using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-List<Produto> produtos =
-[
-    new Produto(){Nome = "Notebook", Preco = 5000, Quantidade = 42},
-    new Produto(){Nome = "Mouse", Preco = 25, Quantidade = 32},
-    new Produto(){Nome = "Teclado", Preco = 50, Quantidade = 22},
-    new Produto(){Nome = "Monitor", Preco = 500, Quantidade = 12},
-    new Produto(){Nome = "Gabinete", Preco = 100, Quantidade = 2},
-];
+// List<Produto> produtos =
+// [
+//     new Produto(){Nome = "Notebook", Preco = 5000, Quantidade = 42},
+//     new Produto(){Nome = "Mouse", Preco = 25, Quantidade = 32},
+//     new Produto(){Nome = "Teclado", Preco = 50, Quantidade = 22},
+//     new Produto(){Nome = "Monitor", Preco = 500, Quantidade = 12},
+//     new Produto(){Nome = "Gabinete", Preco = 100, Quantidade = 2},
+// ];
+
+List<Produto> produtos = new List<Produto>();
 
 app.MapGet("/", () => "API de Produtos!");
 
 app.MapGet("/produto/listar", () => {
-    return Results.Ok(produtos);
-});
-
-app.MapPost("/produto/cadastrar/{nome}/{preco}/{quantidade}", (string nome, float preco, int quantidade) => {
-    Produto produto = new Produto(){Nome = nome, Preco = preco, Quantidade = quantidade};
-    produtos.Add(produto);
-
-    return Results.Ok(produto);
-});
-
-app.MapPost("/produto/cadastrar", async (HttpRequest request) =>
-{
-    var produto = await request.ReadFromJsonAsync<Produto>();
-
-    if (produto == null) {
-        return Results.BadRequest("Dados do produto inválidos");
+    if (produtos.Count() > 0) {
+        return Results.Ok(produtos);
     }
 
+    return Results.NotFound();    
+});
+
+app.MapGet("/produto/buscar/{nome}", (string nome) => {
+    foreach (Produto produtoCadastrado in produtos) {
+        if (produtoCadastrado.Nome == nome) {
+            return Results.Ok(produtoCadastrado);
+        }
+    }
+
+    return Results.NoContent();
+});
+
+app.MapPost("/produto/cadastrar", ([FromBody] Produto produto) => {
+    produtos.Add(produto);
+
+    return Results.Created("", produto);
+});
+// TODO: Ajustar método deletar
+app.MapDelete("/produto/deletar", ([FromBody] Produto produto) => {
+    produtos.Remove(produto);
+
+    return Results.Ok();  
+
+});
+// TODO: Ajustar método atualizar
+app.MapPut("/produto/atualizar", ([FromBody] Produto produto) => {
     produtos.Add(produto);
 
     return Results.Ok(produto);
--- DesenvolvimentoDeSoftwareVisual/api/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ DesenvolvimentoDeSoftwareVisual/api/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8d8ce583f1eb191395a4c4d49d8ebb1846d554d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+551ec2e761c7a0f6f85c997887b73bc32dbb502d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
